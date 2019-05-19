86973632e7fca5c023e51bde4173e6f12f2778b7
Smit Desai <smitdesai@sbcglobal.net>
2019-05-19 02:40:36 -0500

    Setup Login

--- server/index.js
+++ server/index.js
@@ -22,3 +22,18 @@ app.post('/createUser', (req, res) => {
 app.listen(3000, () => {
   console.log('Server running on port 3000')
 })
+
+app.post('/login', (req, res) => {
+  store
+    .authenticate({
+      username: req.body.username,
+      password: req.body.password
+    })
+    .then(({ success }) => {
+      if (success) res.sendStatus(200)
+      else res.sendStatus(401)
+    })
+})
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
+})
