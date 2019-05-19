54094d25da19fe9e64372ab92eb72d6d097210d8
Smit Desai <smitdesai@sbcglobal.net>
2019-05-19 00:37:36 -0500

    Fixing ports

--- server/index.js
+++ server/index.js
@@ -1,5 +1,24 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
+//
+// app.use(express.static(__dirname + '/public'))
+// app.listen(3000, () => console.log('Server running on port 3000'))
 
-app.use(express.static(__dirname + '/public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const store = require('./store')
+const app = express()
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.post('/createUser', (req, res) => {
+  store
+    .createUser({
+      username: req.body.username,
+      password: req.body.password
+    })
+    .then(() => res.sendStatus(200))
+})
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
+})
