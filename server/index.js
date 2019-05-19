b5b063c5e8e6b3cd87e54e30771b17fecd84f4d1
Smit Desai <smitdesai@sbcglobal.net>
2019-05-19 00:43:45 -0500

    Fixing ports

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const store = require('./store')
 const app = express()
-app.use(express.static(__dirname + 'public'))
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
 app.post('/createUser', (req, res) => {
   store
