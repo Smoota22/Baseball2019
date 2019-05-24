5927fb184984addd5cba50da54d462ee9d4fbbbd
Smit Desai <smitdesai@sbcglobal.net>
2019-05-23 23:05:26 -0500

    Updating main page to connect to team data page

--- server/store.js
+++ server/store.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 const knex = require('knex')(require('./knexfile'))
 
 module.exports = {
-  createUser ({first_name, last_name, email, password}) {
+  createUser ({email, password}) {
     console.log(`Add user ${email}`)
     const { salt, hash } = saltHashPassword({ password })
     return knex('user').insert({
