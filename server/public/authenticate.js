0d905aba16b23d15f181264d4e3e86837b9beada
Smit Desai <smitdesai@sbcglobal.net>
2019-05-23 19:08:17 -0500

    Updating main page to connect to team data page

--- server/public/authenticate.js
+++ server/public/authenticate.js
@@ -1,8 +1,8 @@
 const CreateUser = document.querySelector('.CreateUser')
 CreateUser.addEventListener('submit', (e) => {
   e.preventDefault()
-  const first_name = CreateUser.querySelector('.username').value
-  const last_name = CreateUser.querySelector('.password').value
+  const first_name = CreateUser.querySelector('.first_name').value
+  const last_name = CreateUser.querySelector('.last_name').value
   const email = CreateUser.querySelector('.email').value
   const password = CreateUser.querySelector('.password').value
   post('/createUser', { username, password })
