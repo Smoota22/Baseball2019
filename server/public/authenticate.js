0a6573bc09dc292bd32f88aad5a59bc71a410cc3
Smit Desai <smitdesai@sbcglobal.net>
2019-05-23 22:40:59 -0500

    Updating main page to connect to team data page

--- server/public/authenticate.js
+++ server/public/authenticate.js
@@ -1,25 +1,44 @@
+// const CreateUser = document.querySelector('.CreateUser')
+// CreateUser.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   const first_name = CreateUser.querySelector('.first_name').value
+//   const last_name = CreateUser.querySelector('.last_name').value
+//   const email = CreateUser.querySelector('.email').value
+//   const password = CreateUser.querySelector('.password').value
+//   post('/createUser', {first_name, last_name, email, password})
+// })
+//
+// const Login = document.querySelector('.Login')
+// Login.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   const username = Login.querySelector('.username').value
+//   const password = Login.querySelector('.password').value
+//   post('/login', { username, password })
+//     .then(({ status }) => {
+//       if (status === 200) alert('login success')
+//       else alert('login failed')
+//     })
+// })
+//
+// function post (path, data) {
+//   return window.fetch(path, {
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   })
+// }
+
+
 const CreateUser = document.querySelector('.CreateUser')
 CreateUser.addEventListener('submit', (e) => {
   e.preventDefault()
-  const first_name = CreateUser.querySelector('.first_name').value
-  const last_name = CreateUser.querySelector('.last_name').value
-  const email = CreateUser.querySelector('.email').value
+  const username = CreateUser.querySelector('.username').value
   const password = CreateUser.querySelector('.password').value
-  post('/createUser', {first_name, last_name, email, password})
+  post('/createUser', { username, password })
 })
-
-const Login = document.querySelector('.Login')
-Login.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const username = Login.querySelector('.username').value
-  const password = Login.querySelector('.password').value
-  post('/login', { username, password })
-    .then(({ status }) => {
-      if (status === 200) alert('login success')
-      else alert('login failed')
-    })
-})
-
 function post (path, data) {
   return window.fetch(path, {
     method: 'POST',
--- server/store.js
+++ server/store.js
@@ -1,37 +1,67 @@
+// const crypto = require('crypto')
+// const knex = require('knex')(require('./knexfile'))
+//
+// module.exports = {
+//   createUser ({first_name, last_name, email, password}) {
+//     console.log(`Add user ${email}`)
+//     const { salt, hash } = saltHashPassword({ password })
+//     return knex('user').insert({
+//         ID: 0,
+//         first_name: first_name,
+//         last_name: last_name,
+//         email: email,
+//         salt: salt,
+//         encrypted_password: hash
+//   })
+//   },
+//   authenticate ({ username, password }) {
+//     console.log(`Authenticating user ${username}`)
+//     return knex('user').where({ username })
+//       .then(([user]) => {
+//         if (!user) return { success: false }
+//         const { hash } = saltHashPassword({
+//           password,
+//           salt: user.salt
+//         })
+//         return { success: hash === user.encrypted_password }
+//       })
+//   }
+// }
+//
+// function saltHashPassword ({
+//   password,
+//   salt = randomString()
+// }) {
+//   const hash = crypto
+//     .createHmac('sha512', salt)
+//     .update(password)
+//   return {
+//     salt,
+//     hash: hash.digest('hex')
+//   }
+// }
+//
+// function randomString () {
+//   return crypto.randomBytes(4).toString('hex')
+// }
+
+
 const crypto = require('crypto')
 const knex = require('knex')(require('./knexfile'))
-
 module.exports = {
-  createUser ({first_name, last_name, email, password}) {
-    console.log(`Add user ${email}`)
-    const { salt, hash } = saltHashPassword({ password })
+  saltHashPassword,
+  createUser ({ username, password }) {
+    console.log(`Add user ${username}`)
+    const { salt, hash } = saltHashPassword(password)
     return knex('user').insert({
-        ID: 0,
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        salt: salt,
-        encrypted_password: hash
-  })
-  },
-  authenticate ({ username, password }) {
-    console.log(`Authenticating user ${username}`)
-    return knex('user').where({ username })
-      .then(([user]) => {
-        if (!user) return { success: false }
-        const { hash } = saltHashPassword({
-          password,
-          salt: user.salt
-        })
-        return { success: hash === user.encrypted_password }
-      })
+      salt,
+      encrypted_password: hash,
+      username
+    })
   }
 }
-
-function saltHashPassword ({
-  password,
-  salt = randomString()
-}) {
+function saltHashPassword (password) {
+  const salt = randomString()
   const hash = crypto
     .createHmac('sha512', salt)
     .update(password)
@@ -40,7 +70,6 @@ function saltHashPassword ({
     hash: hash.digest('hex')
   }
 }
-
 function randomString () {
   return crypto.randomBytes(4).toString('hex')
 }
