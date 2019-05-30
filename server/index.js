c8dc21a8892b3622c6f7736456802fcfe36dd50c
Smit Desai <smitdesai@sbcglobal.net>
2019-05-29 21:17:29 -0500

    Updating main page to connect to team data page

--- server/index.js
+++ server/index.js
@@ -7,8 +7,24 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mysql = require('mysql')
 const store = require('./store')
 const app = express()
+
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'Baseball_2019_db'
+});
+
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
 app.post('/createUser', (req, res) => {
@@ -39,6 +55,15 @@ app.get('/load_team_data', (req, res) => {
     .load_team_data()
     .then(() => res.sendStatus(200))
 })
+
+app.get('/load_team_data', (req, res) => {
+    let sql = 'SELECT * FROM user';
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send('Posts fetched...');
+    });
+});
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
