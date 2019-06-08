a31cd6c2e30b641d75018b606d8caa5b2d62559a
Smit Desai <smitdesai@sbcglobal.net>
2019-06-08 18:38:18 -0500

    Displaying searched team from route

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.get('/load_team_data/:teamID/:yearID', load_team_data);
 
 function load_team_data(req, res) {
     let sql = 'SELECT games FROM real_team WHERE team_ID = "' + req.params.teamID + '" AND year_ID = ' + req.params.yearID;
-    res.send(sql)
+    // res.send(sql)
     let query = db.query(sql, (err, results) => {
         if(err) throw err;
         console.log(results);
