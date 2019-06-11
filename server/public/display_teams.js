857c2d168fb2b52438ac082afabd08feeb805701
Smit Desai <smitdesai@sbcglobal.net>
2019-06-10 21:58:24 -0500

    Displaying search team from route using inputs

--- server/public/display_teams.js
+++ server/public/display_teams.js
@@ -3,8 +3,7 @@ LoadTeamData.addEventListener('submit', (e) => {
   e.preventDefault()
   const teamID = LoadTeamData.querySelector('.txt_teamID').value
   const yearID = LoadTeamData.querySelector('.txt_yearID').value
-  var promise = post('/load_team_data/submit', {teamID, yearID})
-  var response = promise.json()
+  var response = post('/load_team_data/submit', {teamID, yearID})
   alert(response)
 })
 
@@ -16,5 +15,7 @@ function post (path, data) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
+  }).then(function(response) {
+    return response.json();
   })
 }
