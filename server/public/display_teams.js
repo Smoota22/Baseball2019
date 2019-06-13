ad51b41caa360019164cf002d4d8c1d3dfe6bbb7
Smit Desai <smitdesai@sbcglobal.net>
2019-06-12 20:53:42 -0500

    Updating main page to connect to team data page

--- server/public/display_teams.js
+++ server/public/display_teams.js
@@ -1,63 +1,77 @@
 //Loading team data from inputted team ID and year
-const LoadTeamData = document.querySelector('.LoadTeamData')
-LoadTeamData.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const teamID = LoadTeamData.querySelector('.txt_teamID').value
-  const yearID = LoadTeamData.querySelector('.txt_yearID').value
-  var response = post('/load_team_data/submit', {teamID, yearID})
-  alert(response)
-})
+// const LoadTeamData = document.querySelector('.LoadTeamData')
+// LoadTeamData.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   const teamID = LoadTeamData.querySelector('.txt_teamID').value
+//   const yearID = LoadTeamData.querySelector('.txt_yearID').value
+//   post('/load_team_data/submit', {teamID, yearID})
+// })
 
-// var team_dict = {
-//     "Arizona Diamondbacks" : "ARI",
-//     "Atlanta Braves" : "ATL",
-//     "Baltimore Orioles" : "BAL",
-//     "Boston Red Sox" : "BOS",
-//     "Chicago Cubs" : "CHN",
-//     "Chicago White Sox" : "CHA",
-//     "Cincinnati Reds" : "CIN",
-//     "Cleveland Indians" : "CLE",
-//     "Colorado Rockies" : "COL",
-//     "Detroit Tigers" : "DET",
-//     "Houston Astros" : "HOU",
-//     "Kansas City Royals" : "KCA",
-//     "Los Angeles Angels" : "LAA",
-//     "Los Angeles Dodgers" : "LAN",
-//     "Miami Marlins" : "MIA",
-//     "Milwaukee Brewers" : "MIL",
-//     "Minnesota Twins" : "MIN",
-//     "New York Mets" : "NYN",
-//     "New York Yankees" : "NYA",
-//     "Oakland Athletics" : "OAK",
-//     "Philadelphia Phillies" : "PHI",
-//     "Pittsburgh Pirates" : "PIT",
-//     "San Diego Padres" : "SDN",
-//     "Seattle Mariners" : "SEA",
-//     "San Francisco Giants" : "SFN",
-//     "St. Louis Cardinals" : "SLN",
-//     "Tampa Bay Rays" : "TBA",
-//     "Texas Rangers" : "TEX",
-//     "Toronto Blue Jays" : "TOR",
-//     "Washington Nationals" : "WAS",
-// };
-// var curr_team_name = ""
-// var curr_year = ""
-// $(document).ready(function () {
-//   //Selecting Team From Dropdown
-//   $('ul.dropdown-menu li').click(function() {
-//       curr_team_name = $(this).text();
-//       var curr_team_id =
-//   });
-// });
+var team_dict = {
+    "Arizona Diamondbacks" : "ARI",
+    "Atlanta Braves" : "ATL",
+    "Baltimore Orioles" : "BAL",
+    "Boston Red Sox" : "BOS",
+    "Chicago Cubs" : "CHN",
+    "Chicago White Sox" : "CHA",
+    "Cincinnati Reds" : "CIN",
+    "Cleveland Indians" : "CLE",
+    "Colorado Rockies" : "COL",
+    "Detroit Tigers" : "DET",
+    "Houston Astros" : "HOU",
+    "Kansas City Royals" : "KCA",
+    "Los Angeles Angels" : "LAA",
+    "Los Angeles Dodgers" : "LAN",
+    "Miami Marlins" : "MIA",
+    "Milwaukee Brewers" : "MIL",
+    "Minnesota Twins" : "MIN",
+    "New York Mets" : "NYN",
+    "New York Yankees" : "NYA",
+    "Oakland Athletics" : "OAK",
+    "Philadelphia Phillies" : "PHI",
+    "Pittsburgh Pirates" : "PIT",
+    "San Diego Padres" : "SDN",
+    "Seattle Mariners" : "SEA",
+    "San Francisco Giants" : "SFN",
+    "St. Louis Cardinals" : "SLN",
+    "Tampa Bay Rays" : "TBA",
+    "Texas Rangers" : "TEX",
+    "Toronto Blue Jays" : "TOR",
+    "Washington Nationals" : "WAS",
+};
+var curr_team_id = ""
+var curr_year = ""
+$(document).ready(function () {
+  //Selecting Team From Dropdown
+  $('#team-menu li').click(function() {
+      curr_team_id = team_dict[$(this).text();];
+      if (curr_year != "") {
+          post('/load_team_data/submit', {curr_team_id, curr_year})
+            .then(function(json) {
+              var obj = JSON.parse(json);
+              display_team(obj);
+          })
+      }
+  });
+
+  $('#year-menu li').click(function() {
+      curr_team_name = $(this).text();
+      curr_year =
+      if (curr_team_id != "") {
+          post('/load_team_data/submit', {curr_team_id, curr_year})
+            .then(function(json) {
+              var obj = JSON.parse(json);
+              display_team(obj);
+          })
+      }
+  });
+});
+
+function display_team(jsObj) {
+    alert(jsObj[0].games)
+}
 
 function post (path, data) {
-  var myInit = {
-      method: 'POST',
-      headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-  }}
-
   return window.fetch(path, {
     method: 'POST',
     headers: {
@@ -67,10 +81,11 @@ function post (path, data) {
     body: JSON.stringify(data)
 }).then(function(response) {
     if (response.ok) {
-        return response.text();
+        return response.text();  //To retrieve raw response data
     }
-}).then(function(json) {
-    var obj = JSON.parse(json);
-    alert(obj[0].games);
 })
+// .then(function(json) {
+//     var obj = JSON.parse(json);
+//     alert(obj[0].games);
+// })
 }
