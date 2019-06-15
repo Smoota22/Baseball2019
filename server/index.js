21dde286860ed44199ef8cd65cc76b642d62ddc2
Smit Desai <smitdesai@sbcglobal.net>
2019-06-15 18:45:27 -0500

    fixing login route

--- server/index.js
+++ server/index.js
@@ -75,7 +75,7 @@ function autofill_team_names(req, res) {
 
 app.get('/autofill_years/:teamID', autofill_years);
 function autofill_years(req, res) {
-    let sql = 'SELECT year_ID FROM real_team WHERE team_ID = "' + req.params.teamID;
+    let sql = 'SELECT year_ID FROM real_team WHERE team_ID = "' + req.params.teamID + '"';
     // res.send(sql)
     let query = db.query(sql, (err, results) => {
         if(err) throw err;
