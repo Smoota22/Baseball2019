ed6a31989dd0c9edb3ae6cdce393d203c1ebc0d9
Smit Desai <smitdesai@sbcglobal.net>
2019-06-14 23:20:45 -0500

    fixing login route

--- server/public/authenticate.js
+++ server/public/authenticate.js
@@ -4,10 +4,10 @@ Login.addEventListener('submit', (e) => {
   const email = Login.querySelector('.email').value
   const password = Login.querySelector('.password').value
   post('/login', { email, password })
-    .then(({ status }) => {
-      if (status === 200) alert('login success')
-      else alert('login failed')
-    })
+    // .then(({ status }) => {
+    //   if (status === 200) alert('login success')
+    //   else alert('login failed')
+    // })
     // .then(function(json) {
     //     var obj = JSON.parse(json);
     //     alert(json);
