59174757a729349d25a9de3a7dee8fad137f7fe1
Smit Desai <smitdesai@sbcglobal.net>
2019-06-15 15:24:33 -0500

    fixing login route

--- server/public/display_teams.js
+++ server/public/display_teams.js
@@ -53,9 +53,8 @@ function search_years() {
 
         for (i = 0; i < obj.length; i++) {
 
-            // $name_and_id = str_replace(' ', '_', obj[i].team_name) + "*" + str_replace(' ', '_', obj[i].team_ID);
-            // console.log($name_and_id);
-            // echo "<div class='team_name_item' id=$name_and_id><p>" . $row['name'] . ", " . $row['foodId'] . "</p></div>";
+        //     $name_and_id = str_replace(' ', '_', $row['name']) . "*" . str_replace(' ', '_', $row['foodId']);
+        //     echo "<div class='food_search_item' id=$name_and_id><p>" . $row['name'] . ", " . $row['foodId'] . "</p></div>";
         }
     });
 }
