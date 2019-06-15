e156d22af18be808341b372572d0f3d794440615
Smit Desai <smitdesai@sbcglobal.net>
2019-06-15 16:35:50 -0500

    fixing login route

--- server/public/display_teams.js
+++ server/public/display_teams.js
@@ -50,7 +50,7 @@ function search_years() {
         // alert(json);
         var obj = JSON.parse(json);
         // alert(obj.length);
-        var $log = $("#team_name_form");
+        var $log = $("#display_item_container");
 
         for (i = 0; i < obj.length; i++) {
             var team_name_id = obj[i].team_name + "*" + obj[i].team_ID;
