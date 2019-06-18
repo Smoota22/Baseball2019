4b4f35f7a6a727edd32506704c5284644d443138
Smit Desai <smitdesai@sbcglobal.net>
2019-06-17 21:31:24 -0500

    fixing login route

--- server/public/display_teams.js
+++ server/public/display_teams.js
@@ -146,8 +146,8 @@ function lock_in_season(team_id, team_year) {
             return;
         }
         var query_result_obj = JSON.parse(json);
-        // alert(json["games"]);
-    }
+        alert(json["games"]);
+    });
 }
 
 $(document).ready(function () {
