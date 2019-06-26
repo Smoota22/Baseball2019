6118b7140017d15a3ce13072847fb699bcda055a
Smit Desai <smitdesai@sbcglobal.net>
2019-06-26 01:49:14 -0500

    Setting up table for rankings display

--- server/public/display_teams.js
+++ server/public/display_teams.js
@@ -160,6 +160,8 @@ $(document).ready(function () {
     var search_team_name = $("#search_team").val();
     var search_year = $("#search_team_year").val();
     if (search_team_name != null && search_year != null) {
+        autofill_team_names();
+        $("#search_team_year").val(search_year);
         autofill_years();
     }
 
