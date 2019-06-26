4f5de2ee5b9a1e532ea2ecf4df2abf7cb658f8be
Smit Desai <smitdesai@sbcglobal.net>
2019-06-26 03:27:43 -0500

    Setting up table for rankings display

--- server/index.js
+++ server/index.js
@@ -112,16 +112,16 @@ function ranking(req, res) {
     });
 }
 
-app.get('/ranking_dropdown', ranking_dropdown);
-function ranking_dropodown(req, res) {
-    let sql = 'DESCRIBE real_team';
-    // res.send(sql)
-    let query = db.query(sql, (err, results) => {
-        if(err) throw err;
-        console.log(results);
-        res.send(results);
-    });
-}
+// app.get('/ranking_dropdown', ranking_dropdown);
+// function ranking_dropodown(req, res) {
+//     let sql = 'DESCRIBE real_team';
+//     // res.send(sql)
+//     let query = db.query(sql, (err, results) => {
+//         if(err) throw err;
+//         console.log(results);
+//         res.send(results);
+//     });
+// }
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
