27f582774ba06de663db32c05a05439830113fac
Smit Desai <smitdesai@sbcglobal.net>
2019-06-30 22:34:19 -0500

    Making attribute name change on dropdown

--- server/public/ranking.js
+++ server/public/ranking.js
@@ -136,7 +136,7 @@ function generate_attribute_item(attribute_mysql) {
 
 function set_attribute_dropdown_title() {
     var $ranking_attribute_dropdown_title = $("#ranking_attribute_dropdown_title");
-    $ranking_attribute_dropdown_title.prop('value', ranking_attribute_verbose);
+    $ranking_attribute_dropdown_title.prop('value', 'ranking_attribute_verbose');
 }
 
 function filter_invalid_values() {
