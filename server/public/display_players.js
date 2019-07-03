c3e5a1c6f5b03cd5566a981444e47209e464f7f0
Smit Desai <smitdesai@sbcglobal.net>
2019-07-03 03:24:28 -0500

    Setting up player search

--- server/public/display_players.js
+++ server/public/display_players.js
@@ -338,8 +338,8 @@ $(document).ready(function () {
     });
 
     // $("#search_player").bind("keyup mouseenter", autofill_player_names); //for keyup AND mouse enter/hover
-    $("#search_player").bind("keyup click", autofill_player_names);
+    $("#search_player").bind("keyup click focus", autofill_player_names);
     // $("#search_player_year").bind("keyup mouseenter", autofill_years); //for keyup AND mouse enter/hover
-    $("#search_player_year").bind("keyup click", autofill_player_years);
-    $("#search_player_stint").bind("keyup click", autofill_player_stint);
+    $("#search_player_year").bind("keyup click focus", autofill_player_years);
+    $("#search_player_stint").bind("keyup click focus", autofill_player_stint);
 });
