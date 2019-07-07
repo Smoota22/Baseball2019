97300862dcf9a07ee889a89813d7bd259e80c6e1
Smit Desai <smitdesai@sbcglobal.net>
2019-07-07 02:57:21 -0500

    Setting up player search

--- server/public/display_players.js
+++ server/public/display_players.js
@@ -363,7 +363,7 @@ function reconcile_tables(pitching_length, batting_length, fielding_length) {
         var size = (sum_num / num_tables) * multipliers[i];
 
         var curr_id = "#" + attributes[i] + "_table_div";
-        var curr_class = "col-md-" + sizes[i];
+        var curr_class = "col-md-" + size;
 
         $(curr_id).addclass(curr_class);
     }
