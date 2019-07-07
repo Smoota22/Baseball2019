e7dad56684b443c1a32c16320bdfed3f24fd69b5
Smit Desai <smitdesai@sbcglobal.net>
2019-07-07 02:03:00 -0500

    Setting up player search

--- server/public/display_players.js
+++ server/public/display_players.js
@@ -306,9 +306,9 @@ function lock_in_season() {
     display_stats(player, pitching, batting, fielding);
 }
 
-function call_load_player_stats(table) {
+async function call_load_player_stats(table) {
     var path = '/load_player_stats/' + table + '/' + curr_player_id + '/' + curr_year + '/' + curr_stint + '/' + curr_teamID + '/' + curr_leagueID;
-    var curr = get(path)
+    var curr = await get(path)
     .then(function(json) {
         if (json === undefined) {
             alert("ERROR THIS SHOULD NOT HAPPEN!!!");
@@ -316,10 +316,12 @@ function call_load_player_stats(table) {
         }
 
         var curr = JSON.parse(json);
-        alert(curr);
-        alert(curr[0]);
-        alert(curr[0].player_ID);
     });
+
+
+    alert(curr);
+    alert(curr[0]);
+    alert(curr[0].player_ID);
 
 
     return curr;
