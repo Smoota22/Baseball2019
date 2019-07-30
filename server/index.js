56448855db26f5fcaf19215afbda26165554e560
Smit Desai <smitdesai@sbcglobal.net>
2019-07-30 17:56:46 -0500

    Fixing highlight curr selection

--- server/index.js
+++ server/index.js
@@ -62,9 +62,12 @@ app.post('/login', (req, res) => {
 app.get('/autofill_team_names/:teamName', autofill_team_names);
 function autofill_team_names(req, res) {
     var regex = format_query_string_regex(req.params.teamName);
+    var sql;
     // let sql = 'SELECT team_ID FROM real_team WHERE team_name = "' + req.params.teamName + '"';
     // let sql = 'SELECT team_name FROM real_team WHERE team_name = "sdas"';
-    let sql = 'SELECT team_name, team_ID FROM real_team WHERE team_name LIKE "' + regex + '" GROUP BY team_name, team_ID';
+    if (req.params.teamName != "NULL") {
+        sql = 'SELECT team_name, team_ID FROM real_team WHERE team_name LIKE "' + regex + '" GROUP BY team_name, team_ID';
+    }
     // res.send(sql);
     let query = db.query(sql, (err, results) => {
         if(err) throw err;
